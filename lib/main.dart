cb184468f435717f246ec5648b9e859d4e1e4ab3
jefflueck <lueckcfd@yahoo.com>
2024-07-21 19:38:56 -0500

    updated pubspec.yaml, updated main.dart, updated start_screen.dart, updated .gitignore, new home screen created with images, text, and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.purple],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
     ),
   );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the Fun Way!',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {
+              print('Start Quiz button pressed');
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
