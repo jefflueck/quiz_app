cc88c0426804e107cc5444e279930dba1e079bee
jefflueck <lueckcfd@yahoo.com>
2024-07-25 17:30:27 -0500

    broke out buttons on questoins_screen.dart into a seperate Stateless Widget called answer_button.dart. Added styling to answer_button.dart, added named required named arguments so we can dynamically pull in questions and answers from differnt files in the app in answer_button.dart rendering dynamically.

---
+++ lib/answer_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      // * we could also have these as positional arguments verses named arguments if we choose. ex (this.answerText, this.onTap, {super.key} ) In this example the first two are now positional arguments and would also work in this widget.
+      // ! If we choose to use the named arguments we must also use the {} to wrap the arguments, and must add the required front of the arguments in this implementation.
+      {required this.answerText,
+      required this.onTap,
+      super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        foregroundColor: Colors.blue,
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/answer_button.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
   @override
@@ -11,18 +13,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(
+    return SizedBox(
+      width: double.infinity,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Questions Screen',
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-            ),
+          const Text(
+            'The Question',
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
           const SizedBox(height: 30),
+          AnswerButton(
+            // * Because we chose to use named arguments in the AnswerButton widget, we must add the required answerText in front of the arguments in this implementation.
+            answerText: 'Answer 1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 4',
+            onTap: () {},
+          ),
         ],
       ),
     );
