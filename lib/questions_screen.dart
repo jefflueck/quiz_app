3792b185c3d0e9cc05315d4ac10085600def2876
jefflueck <lueckcfd@yahoo.com>
2024-07-28 17:18:48 -0500

    imported questions.dart file for questions and answers, starting to dynamically display qestions from file

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,31 +14,32 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestions = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+          Text(
+            currentQuestions.text,
+            style: const TextStyle(fontSize: 16, color: Colors.white),
           ),
           const SizedBox(height: 30),
           AnswerButton(
             // * Because we chose to use named arguments in the AnswerButton widget, we must add the required answerText in front of the arguments in this implementation.
-            answerText: 'Answer 1',
+            answerText: currentQuestions.answers[0],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: 'Answer 2',
+            answerText: currentQuestions.answers[1],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: 'Answer 3',
+            answerText: currentQuestions.answers[2],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: 'Answer 4',
+            answerText: currentQuestions.answers[3],
             onTap: () {},
           ),
         ],
